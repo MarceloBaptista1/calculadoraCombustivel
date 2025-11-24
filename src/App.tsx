5c47027608d59bfc1eb5f47a38a577ac6dc021a4
Marcelo Henrique Baptista <m_baptista2@icloud.com>
2025-11-24 19:50:07 -0300

    parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import { useState } from "react";
+import logoImg from "./assets/logo.png";
 
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
     return (
         <div>
-            <h1>teste</h1>
+            <main className="container">
+                <img
+                    className="logo"
+                    src={logoImg}
+                    alt="Logo de uma bomba de combustivel"
+                />
+                <h1 className="title">Qual Melhor Opção?</h1>
+
+                <form className="form">
+                    <label> Álcool (Preço por litro)</label>
+                    <input
+                        className="input"
+                        type="nuumber"
+                        placeholder="Qual valor do Álcool em sua cidade?"
+                        min="1"
+                        step="0.01"
+                        required
+                    />
+                    <label> Gasolina (Preço por litro)</label>
+                    <input
+                        className="input"
+                        type="nuumber"
+                        placeholder="Qual valor da Gasolina em sua cidade?"
+                        min="1"
+                        step="0.01"
+                        required
+                    />
+
+                    <input className="button" type="submit" value="Calcular" />
+                </form>
+            </main>
         </div>
     );
 }
